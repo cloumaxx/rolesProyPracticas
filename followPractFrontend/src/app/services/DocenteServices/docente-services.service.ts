eaad499328f9f7c46fccb9791fb819a945904a06
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2023-10-04 14:57:35 -0500

    aha

--- followPractFrontend/src/app/services/DocenteServices/docente-services.service.ts
+++ followPractFrontend/src/app/services/DocenteServices/docente-services.service.ts
@@ -29,5 +29,34 @@ export class DocenteService {
     const url = `${this.baseUrl}docentes_list/`;
     return this.http.get(url);
   }
+  registrarDocente(docente: any) {
+    const url = `${this.baseUrl}crear_docente/`;
+
+    return this.http.post(url, docente);
+  }
+
+  obtenerListaDocentes() {
+    const url = `${this.baseUrl}listar_docentes/`;
+
+    return this.http.get(url);
+  }
+
+  obtenerDocentePorId(docenteId: number) {
+    const url = `${this.baseUrl}obtener_docente/${docenteId}/`;
+
+    return this.http.get(url);
+  }
+
+  actualizarDocente(docenteId: number, docente: any) {
+    const url = `${this.baseUrl}actualizar_docente/${docenteId}/`;
+
+    return this.http.put(url, docente);
+  }
+
+  eliminarDocente(docenteId: number) {
+    const url = `${this.baseUrl}eliminar_docente/${docenteId}/`;
+
+    return this.http.delete(url);
+  }
 
 }
