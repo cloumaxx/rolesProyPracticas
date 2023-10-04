f05168c92376aaff1e0e0975a30879956ef7daec
soto0113 <83653740+soto0113@users.noreply.github.com>
2023-10-04 14:24:52 -0500

    --

--- followPractFrontend/src/app/body/forms/formulario-nuevo-docente-monitor/formulario-nuevo-docente-monitor.component.ts
+++ followPractFrontend/src/app/body/forms/formulario-nuevo-docente-monitor/formulario-nuevo-docente-monitor.component.ts
@@ -8,22 +8,22 @@ import { DocenteService } from 'src/app/services/DocenteServices/docente-service
   styleUrls: ['./formulario-nuevo-docente-monitor.component.css']
 })
 export class FormularioNuevoDocenteMonitorComponent {
-  docenteForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder, private docenteService: DocenteService) {
-    this.docenteForm = this.formBuilder.group({
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      cedula: ['', Validators.required],
-      correo: ['', Validators.required],
-      // Agrega más campos según tus necesidades
-    });
-  }
+  docente = {
+    nombre : '',
+    apellido : '',
+    cedula : '',
+    correoPersonal : '',
+    correoInstitucional : '',
+    contrasena : '',
+    fechaNacimiento : '',
+    estado : true,
+    horasDispobibles : 0
+  };
+  constructor(private docenteService: DocenteService) {};
+  
 
   registrarDocente() {
-    if (this.docenteForm.valid) {
-      const docenteData = this.docenteForm.value;
-      this.docenteService.registrarDocente(docenteData).subscribe(
+      this.docenteService.registrarDocente(this.docente).subscribe(
         (response) => {
           console.log('Docente registrado exitosamente:', response);
           // Puedes redirigir a otra página, mostrar un mensaje de éxito, etc.
@@ -33,9 +33,5 @@ export class FormularioNuevoDocenteMonitorComponent {
           // Manejar el error, mostrar un mensaje de error, etc.
         }
       );
-    } else {
-      console.error('Formulario no válido. Por favor, complete todos los campos requeridos.');
-      // Puedes mostrar un mensaje al usuario indicando que debe completar todos los campos requeridos.
-    }
-  }
+      }
 }
